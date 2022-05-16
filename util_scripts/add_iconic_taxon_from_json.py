b378a07634dba5e179c912e2c650b0a40a62479c
aescobarr <agustiescobar@gmail.com>
2022-05-16 09:27:07 +0200

    added iconic taxon name and id

---
+++ util_scripts/add_iconic_taxon_from_json.py
@@ -0,0 +1,17 @@
+import init_project
+import os
+import json
+from data_aggregator_api import settings
+
+
+PATH = str(settings.BASE_DIR) + "/"
+
+result = [os.path.join(dp, f) for dp, dn, filenames in os.walk(PATH) for f in filenames if os.path.splitext(f)[1] == '.json']
+for file in result:
+    json_data = open(file)
+    data = json.load(json_data)
+    for d in data:
+        id = d['id']
+        taxon_id = d['taxon']['iconic_taxon_id']
+        taxon = d['taxon']['iconic_taxon_name']
+        print("update main_observation set iconic_taxon_id={0}, iconic_taxon_name='{1}' where native_id='{2}';".format(taxon_id,taxon,id))
